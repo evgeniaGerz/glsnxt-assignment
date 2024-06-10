b29ce535953ff45b04aad4ca13c114c5cc91a5fc
Evgenia Gertsa <topuzzino@hotmail.de>
2024-06-10 23:04:47 +0200

    Add close x button

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,23 +1,30 @@
-import { createPortal } from 'react-dom';
-import './Modal.css'
-
+import { createPortal } from "react-dom";
+import "./Modal.css";
+import closeIcon from "./assets/times-solid.svg";
 
 function Modal({ onModalClose }) {
   return (
-    <div style={{ border: '2px solid black' }}>
+    <div style={{ border: "2px solid black" }}>
       {createPortal(
-      <div className='modal-wrapper' role="dialog"><div className='modal-content'>
-        <header>title will be added here</header>
-        <div>this is a modal's content</div>
-        <footer>
-          <button>Save</button>
-          <button onClick={onModalClose}>Close</button>
-        </footer>
-        </div></div>,
-      document.body
-    )}
+        <div className="modal-wrapper" role="dialog" aria-modal="true">
+          <div className="modal-content">
+            <header>
+              <h2>title goes here</h2>
+              <button onClick={onModalClose}>
+                <img width={30} src={closeIcon} />
+              </button>
+            </header>
+            <main>this is a modal's content</main>
+            <footer>
+              <button onClick={onModalClose}>Close</button>
+              <button>Save</button>
+            </footer>
+          </div>
+        </div>,
+        document.body
+      )}
     </div>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
