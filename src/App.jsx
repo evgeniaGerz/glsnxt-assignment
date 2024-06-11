eedfb85279cce191e76c14cd572be250abd37b3f
Evgenia Gertsa <topuzzino@hotmail.de>
2024-06-11 13:00:20 +0200

    Remove unused styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import { useState } from 'react'
-import glsnextLogo from './assets/glsnxt-logo.svg'
-import './App.css'
-import Modal from './Modal'
+import { useState } from "react"
+import glsnextLogo from "./assets/glsnxt-logo.svg"
+import "./App.css"
+import Modal from "./Modal"
 
 function App() {
   const [isModalVisible, setIsModalVisible] = useState(false)
 
   return (
-    <>
-    <div className='page-wrapper'>
-      <div className='container'>
+    <div className="app">
+      {/* <div className="container">
         <a href="https://www.glsnxt.com/" target="_blank">
           <img src={glsnextLogo} className="logo" alt="glsnxt logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setIsModalVisible(true)}>
-          open modal
-        </button>
-      </div>
-      {isModalVisible && <Modal onModalClose={() => setIsModalVisible(false)} />}
-
+        </a>∏
+      </div> */}
+      <button
+        className="openModal-button"
+        onClick={() => setIsModalVisible(true)}
+      >
+        open modal
+      </button>
+      {isModalVisible && (
+        <Modal onModalClose={() => setIsModalVisible(false)} />
+      )}
     </div>
-    </>
   )
 }
 
