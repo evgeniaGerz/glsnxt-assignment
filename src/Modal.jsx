c0990ba117fc06997f52ced6ab30e8253d3e0590
Evgenia Gertsa <topuzzino@hotmail.de>
2024-06-11 13:54:59 +0200

    Style modal content

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,30 +1,28 @@
-import { createPortal } from "react-dom";
-import "./Modal.css";
-import closeIcon from "./assets/times-solid.svg";
+import { createPortal } from "react-dom"
+import "./Modal.css"
+import closeIcon from "./assets/times-solid.svg"
 
 function Modal({ onModalClose }) {
-  return (
-    <div style={{ border: "2px solid black" }}>
-      {createPortal(
-        <div className="modal-wrapper" role="dialog" aria-modal="true">
-          <div className="modal-content">
-            <header>
-              <h2>title goes here</h2>
-              <button onClick={onModalClose}>
-                <img width={30} src={closeIcon} />
-              </button>
-            </header>
-            <main>this is a modal's content</main>
-            <footer>
-              <button onClick={onModalClose}>Close</button>
-              <button>Save</button>
-            </footer>
-          </div>
-        </div>,
-        document.body
-      )}
-    </div>
-  );
+  return createPortal(
+    <>
+      <div onClick={onModalClose} className="modal-container">
+        <div className="modal" role="dialog" aria-modal="true">
+          <header>
+            <h2>title goes here</h2>
+            <button className="close-icon" onClick={onModalClose}>
+              <img width={30} src={closeIcon} />
+            </button>
+          </header>
+          <main>this is a modal's content</main>
+          <footer>
+            <button onClick={onModalClose}>Close</button>
+            <button>Save</button>
+          </footer>
+        </div>
+      </div>
+    </>,
+    document.body
+  )
 }
 
-export default Modal;
+export default Modal
